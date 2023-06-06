22a53f76a8ef5d49a69ed1849650a92802b40820
zian <mynameiszian@gmail.com>
2023-06-06 16:00:17 +0800

    re impl balance transfer constructs fn

--- src/extrinsics/transaction_constructor/transfer_balance/constructor.rs
+++ src/extrinsics/transaction_constructor/transfer_balance/constructor.rs
@@ -1,22 +1,66 @@
 use std::str::FromStr;
 
-use subxt::{tx::DynamicTxPayload, utils::AccountId32};
+use subxt::{
+    tx::{DynamicTxPayload, StaticTxPayload},
+    utils::{AccountId32, MultiAddress},
+};
 
-use crate::extrinsics::prelude::GenericError;
+use crate::extrinsics::{
+    prelude::{BlockchainClient, GenericError},
+    transaction_constructor::traits::ValidateHash,
+};
+
+#[derive(subxt::ext::codec::Encode, subxt::ext::codec::Decode)]
+struct BalanceTransferArgs {
+    pub dest: MultiAddress<AccountId32, ()>,
+    pub value: u128,
+}
+
+impl BalanceTransferArgs {
+    fn new(dest: MultiAddress<AccountId32, ()>, value: u128) -> Self {
+        Self { dest, value }
+    }
+}
 
 pub struct BalanceTransfer;
 
-type BalanceTransferPayload = DynamicTxPayload<'static>;
+impl BalanceTransfer {
+    fn generate_payload(
+        client: BlockchainClient,
+        args: BalanceTransferArgs,
+    ) -> BalanceTransferPayload {
+        subxt::tx::StaticTxPayload::new(
+            Self::pallet_name(),
+            Self::function_name(),
+            args,
+            Self::call_hash(client),
+        )
+    }
+}
+
+impl ValidateHash for BalanceTransfer {
+    fn pallet_name() -> &'static str {
+        "Balances"
+    }
+
+    fn function_name() -> &'static str {
+        "transfer_keep_alive"
+    }
+}
+
+type BalanceTransferPayload = StaticTxPayload<BalanceTransferArgs>;
 
 impl BalanceTransfer {
-    pub fn construct(to: &str, value: u128) -> Result<BalanceTransferPayload, GenericError> {
+    pub fn construct(
+        to: &str,
+        value: u128,
+        client: BlockchainClient,
+    ) -> Result<BalanceTransferPayload, GenericError> {
         let dest = subxt::utils::MultiAddress::Id(AccountId32::from_str(to)?);
 
-        let payload = subxt::dynamic::tx("Balances", call_name, fields);
+        let args = BalanceTransferArgs::new(dest, value);
 
-        let payload = runtime_types::api::tx()
-            .balances()
-            .transfer_keep_alive(dest, value);
+        let payload = Self::generate_payload(client, args);
 
         Ok(payload)
     }
