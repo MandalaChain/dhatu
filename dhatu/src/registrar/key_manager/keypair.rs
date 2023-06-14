1c81908fd5567ea7b4d064e7809bc9aa9d1b1ecd
ramandabhijana <abhijanaramanda@gmail.com>
2023-06-14 20:26:43 +0800

    update test_from_str_valid body

--- dhatu/src/registrar/key_manager/keypair.rs
+++ dhatu/src/registrar/key_manager/keypair.rs
@@ -361,60 +361,9 @@ mod private_key_tests {
 
     #[test]
     fn test_from_str_valid() {
-        
-        // let mini_secret_key: MiniSecretKey = MiniSecretKey::generate();
-        // let secret_key: SecretKey = mini_secret_key.expand(MiniSecretKey::ED25519_MODE);
-        // let secret_key_bytes: [u8; 64] = secret_key.to_bytes();
-
-        // panic!("secret_key: {:?}", SchnorrkelKeypair::generate().secret);
-
-        // let valid_key_str = key_bytes.to_bytes().iter().map(|&c| c as char).collect::<String>();
-
-        // let valid_key_str = str::from_utf8(&key_bytes).unwrap();
-        // let valid_key_str = String::from_utf8(key_bytes[0..32].to_vec()).unwrap();
-
-        // let valid_key_str = base64::encode(&secret_key_bytes);
-
-        // let keypair_bytes = hex!("28b0ae221c6bb06856b287f60d7ea0d98552ea5a16db16956849aa371db3eb51fd190cce74df356432b410bd64682309d6dedb27c76845daf388557cbac3ca3446ebddef8cd9bb167dc30878d7113b7e168e6f0646beffd77d69d39bad76b47a");
-        // let keypair: Keypair = Keypair::from_bytes(&keypair_bytes[..]).unwrap();
-        
-        let bytes = Keypair::generate().secret.to_bytes();
-        // let vec = bytes.to_vec();
-        // panic!("secret_key: {:?}", );
-
-        // Convert bytes to string
-        let encoded_string = base64::encode(&bytes);
-
-        let as_bytes = encoded_string.as_bytes();
-
-        let str = str::from_utf8(as_bytes).unwrap();
-
-        // Convert string back to bytes
-        let decoded_bytes = base64::decode(&encoded_string).unwrap();
-
-        let sl = decoded_bytes.as_slice();
-
-        // Ensure the decoded bytes match the original bytes
-        assert_eq!(bytes, decoded_bytes.as_slice());
-
-        // panic!("Encoded string: {}", encoded_string);
-        println!("Decoded bytes: {:?}", decoded_bytes);
-
-
-        // println!("valid_key_str: {valid_key_str}");
-
-        let result = PrivateKey::from_str(str).unwrap();
-        // assert!(result.is_ok());
-
-        /*
-        // Generate a new random Schnorrkel keypair
-        let keypair = SchnorrkelKeypair::generate();
-
-        // Extract the private key from the keypair
-        let private_key = keypair.secret.to_bytes().to_vec();
-
-        panic!("Private Key: {:?}", base64::encode(&private_key));
-        */
+        let mini_secret_key = "0xd5836897dc77e6c87e5cc268abaaa9c661bcf19aea9f0f50a1e149d21ce31eb7";
+        let result = PrivateKey::from_str(mini_secret_key);
+        assert!(result.is_ok());
     }
 
     #[test]
