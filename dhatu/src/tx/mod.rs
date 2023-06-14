da56db8a3c58405e2649dd130724f60d7ae2ecaa
zian <mynameiszian@gmail.com>
2023-06-15 00:53:07 +0800

    done documenting dhatu assets

--- dhatu/src/tx/mod.rs
+++ dhatu/src/tx/mod.rs
@@ -1,2 +1,6 @@
+//! Transaction module. this modules mostly consist of transaction abstraction
+
+/// extrinsics transaction abstraction
 pub mod extrinsics;
+/// specialized modules used for managing mandala blockchain nft assets
 pub mod dhatu_assets;
