8d03c3c87e6bb9ae44fd8af5e41d586a8effecae
ramandabhijana <abhijanaramanda@gmail.com>
2023-06-14 12:20:25 +0800

    add unit tests to KeyManager

--- dhatu/src/registrar/key_manager/mod.rs
+++ dhatu/src/registrar/key_manager/mod.rs
@@ -57,3 +57,49 @@ impl KeyManager {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_recover_valid() {
+        let pass = "61c510ba04db830da8acd6595caf193d";
+        let phrase = "endorse doctor arch helmet master dragon wild favorite property mercy vault maze";
+        
+        let keypair_result = KeyManager::recover(pass, phrase);
+        assert!(keypair_result.is_ok());
+        
+        let keypair = keypair_result.unwrap();
+        assert_eq!(keypair.password_hash().as_str(), pass);
+        assert_eq!(keypair.phrase().inner(), phrase);
+    }
+    
+    #[test]
+    fn test_recover_invalid_phrase() {
+        let pass = "483d968823979f7a937c65793ee91409";
+        let phrase = "sample tornado pen frog valley library velvet figure guitar powder mirror churne";
+        let keypair_result = KeyManager::recover(pass, phrase);
+        assert!(keypair_result.is_err());
+    }
+
+    #[test]
+    fn test_recover_invalid_pass() {
+        let pass = "61457fa9cd845bb9";
+        let phrase = "endorse doctor arch helmet master dragon wild favorite property mercy vault maze";
+        let keypair_result = KeyManager::recover(pass, phrase);
+        assert!(keypair_result.is_err());
+    }
+    
+    #[test]
+    fn test_gen_from_phrase() {
+        let password = Password::new();
+        let (_, phrase, _) = Keys::generate_with_phrase(password.as_pwd());
+        
+        let keypair_result = KeyManager::gen_from_phrase(password.clone(), phrase.as_str());
+        assert!(keypair_result.is_ok());
+        
+        let keypair = keypair_result.unwrap();
+        assert_eq!(keypair.password_hash().as_str(), password.as_str());
+        assert_eq!(keypair.phrase().inner(), phrase);
+    }
+}
