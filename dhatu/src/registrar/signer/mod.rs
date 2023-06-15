c6ab0887e0414853b420194a898813fb18ccd1f9
ramandabhijana <abhijanaramanda@gmail.com>
2023-06-15 11:03:16 +0800

    finish signer unit test

--- dhatu/src/registrar/signer/mod.rs
+++ dhatu/src/registrar/signer/mod.rs
@@ -42,93 +42,6 @@ impl TxBuilder {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use std::str::FromStr;
-
-//     use sp_core::{crypto::Ss58Codec, Pair};
-//     use subxt::{
-//         rpc::types::DryRunError,
-//         utils::{AccountId32, MultiAddress},
-//     };
-
-//     use super::*;
-
-//     async fn mock_client() -> crate::types::NodeClient {
-//         OnlineClient::<PolkadotConfig>::new().await.unwrap()
-//     }
-
-//     fn mock_payload(
-//         client: &crate::types::NodeClient,
-//     ) -> subxt::tx::StaticTxPayload<runtime_types::api::balances::calls::Transfer> {
-//         let _metadata = client.metadata();
-
-//         let dest = mock_acc();
-
-//         runtime_types::api::tx().balances().transfer(dest, 0)
-//     }
-
-//     fn mock_acc() -> MultiAddress<AccountId32, ()> {
-//         let (dest, _) = sp_core::sr25519::Pair::generate();
-//         let dest = dest.public();
-//         let dest = AccountId32::from_str(&dest.to_ss58check()).unwrap();
-
-//         subxt::utils::MultiAddress::Id(dest)
-//     }
-
-//     fn mock_pair() -> sp_core::sr25519::Pair {
-//         let (pair, _) = sp_core::sr25519::Pair::generate();
-
-//         pair
-//     }
-
-//     #[actix::test]
-//     async fn should_create_unsigned_tx() {
-//         let node_client = mock_client().await;
-//         let payload = mock_payload(&node_client);
-
-//         let extrinsic = TxBuilder::unsigned(&node_client, &payload).unwrap();
-
-//         let dry_run_result = extrinsic.dry_run(None).await.unwrap();
-//         let actual_result = extrinsic.submit().await;
-
-//         // should error because the transaction is unsigned and can only be
-//         // submitted through OCW
-//         // but it should be possible to include the transaction in the block.
-//         // that's why instead of validity erorr it's dispatch error
-//         if let Err(dry_run_result) = dry_run_result {
-//             assert_eq!(dry_run_result, DryRunError::DispatchError);
-//         }
-
-//         if let Err(actual_result) = actual_result {
-//             println!("{}", actual_result)
-//         }
-//     }
-
-//     #[actix::test]
-//     async fn should_create_signed_tx() {
-//         let node_client = mock_client().await;
-//         let payload = mock_payload(&node_client);
-
-//         let pair = mock_pair();
-//         let extrinsic = TxBuilder::signed(&node_client, pair, &payload)
-//             .await
-//             .unwrap();
-
-//         let dry_run_result = extrinsic.dry_run(None).await.unwrap();
-//         let actual_result = extrinsic.submit().await;
-
-//         // shoould error because the caller does not have enough balance
-//         if let Err(dry_run_result) = dry_run_result {
-//             assert_eq!(dry_run_result, DryRunError::TransactionValidityError);
-//         }
-
-//         if let Err(actual_result) = actual_result {
-//             println!("{}", actual_result)
-//         }
-//     }
-// }
-
 #[cfg(test)]
 mod tests {
     use subxt::{PolkadotConfig, SubstrateConfig, error::DispatchError};
@@ -224,20 +137,8 @@ mod tests {
             .await
             .unwrap();
 
-        // let dry_run_result = extrinsic.dry_run(None).await.unwrap();
-        let actual = extrinsic.submit().await.unwrap();
-
-        // assert!(actual_result.is_ok());
-
-        println!("{:?}", actual);
-
-        // shoould error because the caller does not have enough balance
-        // if let DryRunResult::DispatchError(err) = dry_run_result {
-        //     assert_eq!(err, DispatchError::Other);
-        // }
-
-        // if let Err(actual_result) = actual_result {
-        //     println!("{}", actual_result)
-        // }
+        let dry_run_result = extrinsic.dry_run(None).await.unwrap();
+        let actual_result = extrinsic.submit().await;
+        assert!(actual_result.is_ok());
     }
 }
