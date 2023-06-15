60b1f0fcf2b158420aee7f6ae14727d34ff53bfb
ramandabhijana <abhijanaramanda@gmail.com>
2023-06-15 08:56:49 +0800

    add create_unsigned_tx unit test (not passed yet)

--- dhatu/src/registrar/signer/mod.rs
+++ dhatu/src/registrar/signer/mod.rs
@@ -128,3 +128,109 @@ impl TxBuilder {
 //         }
 //     }
 // }
+
+#[cfg(test)]
+mod tests {
+    use subxt::{PolkadotConfig, SubstrateConfig, error::DispatchError};
+    pub(crate) use subxt::OnlineClient;
+    use std::str::FromStr;
+
+    use sp_core::{crypto::Ss58Codec, Pair};
+    use subxt::{
+        utils::{AccountId32, MultiAddress},
+        rpc::types::DryRunResult
+    };
+
+    use super::*;
+
+    async fn mock_client() -> crate::types::NodeClient {
+        OnlineClient::<SubstrateConfig>::new().await.unwrap()
+    }
+
+    // Generate an interface that we can use from the node's metadata.
+    #[subxt::subxt(runtime_metadata_path = "./src/registrar/signer/polkadot_metadata_small.scale")]
+    pub mod polkadot {}
+
+    // Mock implementation of `WrappedExtrinsic` for testing
+    struct MockWrappedExtrinsic<T: EncodeAsFields>(subxt::tx::Payload<T>);
+
+    impl<T: EncodeAsFields> WrappedExtrinsic<T> for MockWrappedExtrinsic<T> {
+        fn into_inner(self) -> subxt::tx::Payload<T> {
+            self.0
+        }
+    }
+
+    fn mock_payload(
+        client: &crate::types::NodeClient,
+    ) -> MockWrappedExtrinsic<polkadot::balances::calls::types::Transfer> {
+        let _metadata = client.metadata();
+
+        let dest = mock_acc();
+
+        MockWrappedExtrinsic(polkadot::tx().balances().transfer(dest, 10_000))
+    }
+
+    fn mock_acc() -> MultiAddress<AccountId32, ()> {
+        let (dest, _) = sp_core::sr25519::Pair::generate();
+        let dest = dest.public();
+        let dest = AccountId32::from_str(&dest.to_ss58check()).unwrap();
+
+        subxt::utils::MultiAddress::Id(dest)
+    }
+
+    fn mock_pair() -> sp_core::sr25519::Pair {
+        let (pair, _) = sp_core::sr25519::Pair::generate();
+
+        pair
+    }
+
+    #[tokio::test]
+    async fn should_create_unsigned_tx() {
+        let node_client = mock_client().await;
+        let payload = mock_payload(&node_client);
+
+        let extrinsic = TxBuilder::unsigned(&node_client, payload).unwrap();
+
+        let dry_run_result: DryRunResult  = extrinsic.dry_run(None).await.unwrap();
+        let actual_result = extrinsic.submit().await;
+
+        // should error because the transaction is unsigned and can only be
+        // submitted through OCW
+        // but it should be possible to include the transaction in the block.
+        // that's why instead of validity erorr it's dispatch error
+        if let DryRunResult::DispatchError(err) = dry_run_result {
+            assert_eq!(
+                format!("{:?}", err),
+                format!("{:?}", DispatchError::Other)
+            );
+        }
+
+
+        if let Err(actual_result) = actual_result {
+            println!("{}", actual_result)
+        }
+    }
+
+    // #[actix::test]
+    // async fn should_create_signed_tx() {
+    //     let node_client = mock_client().await;
+    //     let payload = mock_payload(&node_client);
+
+    //     let pair = mock_pair();
+    //     let extrinsic = TxBuilder::signed(&node_client, pair, &payload)
+    //         .await
+    //         .unwrap();
+
+    //     let dry_run_result = extrinsic.dry_run(None).await.unwrap();
+    //     let actual_result = extrinsic.submit().await;
+
+    //     // shoould error because the caller does not have enough balance
+    //     if let Err(dry_run_result) = dry_run_result {
+    //         assert_eq!(dry_run_result, DryRunError::TransactionValidityError);
+    //     }
+
+    //     if let Err(actual_result) = actual_result {
+    //         println!("{}", actual_result)
+    //     }
+    // }
+}
