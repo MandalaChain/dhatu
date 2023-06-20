bf95349d7ada2246e8f568d9b149301818f178bd
zian <mynameiszian@gmail.com>
2023-06-20 13:01:06 +0800

    add setup

---
+++ dhatu/tests/common/mod.rs
@@ -0,0 +1,18 @@
+use dhatu;
+use mandala_node_runner;
+
+pub fn setup() -> (
+    dhatu::types::MandalaClient,
+    mandala_node_runner::SubstrateNode,
+) {
+    let madya_bin_path = std::env::var("MADYA_BIN_PATH")
+        .expect("madya node binary path (MADYA_BIN_PATH) must be set!");
+    let mut node = mandala_node_runner::SubstrateNodeBuilder::new();
+
+    let node = node
+        .binary_path(madya_bin_path)
+        .spawn()
+        .expect("should spawn a new development node!");
+
+    let node_url = format!("127.0.0.1:{}", node.ws_port());
+}
