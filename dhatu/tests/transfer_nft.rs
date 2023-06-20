937ddb8b9b5b29aeb076a0cfa1a5334085a4c945
zian <mynameiszian@gmail.com>
2023-06-20 15:50:56 +0800

    partially make function to test transfer nft

--- dhatu/tests/transfer_nft.rs
+++ dhatu/tests/transfer_nft.rs
@@ -1,9 +1,13 @@
 mod common;
 
-use dhatu;
+use dhatu::{
+    self, tx::extrinsics::transaction_constructor::transfer_nft_contract::constructor::TransferNFT,
+};
 use mandala_node_runner;
 
 #[tokio::test]
 async fn should_transfer_nft() {
-    todo!()
+    let (client, _node) = common::setup_node_and_client().await;
+    let contract = common::setup_dummy_721_contract(&client).await;
+    let payload = TransferNFT::construct(address, to, token_id, function_selector);
 }
