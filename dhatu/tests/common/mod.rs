af783ddbb7659791e9f64d58d45c1baf929c36cf
zian <mynameiszian@gmail.com>
2023-06-20 14:18:40 +0800

    added dummy erc 721 contract metadata and wasm binary

--- dhatu/tests/common/mod.rs
+++ dhatu/tests/common/mod.rs
@@ -1,5 +1,8 @@
 use dhatu::{self, types::MandalaClient};
 use mandala_node_runner;
+use subxt::{tx::PairSigner, PolkadotConfig};
+
+use self::test_types::api::contracts::events::Instantiated;
 mod test_types;
 
 pub fn setup_node_and_client() -> (
@@ -18,17 +21,34 @@ pub fn setup_node_and_client() -> (
     let node_url = format!("127.0.0.1:{}", node.ws_port());
 }
 
-pub async fn setup_dummy_721_contract(client:MandalaClient) {
+pub async fn setup_dummy_721_contract(client: MandalaClient) {
+    
     let tx_payload = test_types::api::tx().contracts().instantiate_with_code(
-        value,
-        gas_limit,
-        storage_deposit_limit,
+        0 , // transfered value
+        20_000_000_000, // static gas limit
+        None, // storage deposit limit 
         code,
         data,
         salt,
     );
 
-    client.
+    let signer = sp_keyring::Sr25519Keyring::Alice.pair();
+    let signer: PairSigner<PolkadotConfig, sp_core::sr25519::Pair> = PairSigner::new(signer);
+
+    let deploy_result = client
+        .inner()
+        .tx()
+        .sign_and_submit_then_watch_default(call, &signer)
+        .await
+        .expect("should deploy a new dummy contract transaction successfuly! ")
+        .wait_for_finalized_success()
+        .await
+        .expect("should deploy dummy contract successfully");
+
+    let contract = deploy_result
+        .find_first::<Instantiated>()
+        .expect("should emit instantiated event")
+        .expect("should find instantiated event");
 
 
 }
