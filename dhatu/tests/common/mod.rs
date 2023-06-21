933953bb281ca7bee8e951bdd5fe522c1ad39708
zian <mynameiszian@gmail.com>
2023-06-21 19:58:49 +0800

    deploy test contrac on the fly

--- dhatu/tests/common/mod.rs
+++ dhatu/tests/common/mod.rs
@@ -17,6 +17,7 @@ use dhatu::{
 };
 use mandala_node_runner;
 use parity_scale_codec::{Compact, Encode};
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
 use sp_core::sr25519::Pair;
 use subxt::{tx::PairSigner, utils::AccountId32, PolkadotConfig};
 
@@ -36,6 +37,7 @@ pub const STATIC_GAS_LIMIT: Weight = Weight {
 };
 
 const STATIC_MINT_STORAGE_DEPOSIT_LIMIT: Option<Compact<u128>> = Some(Compact(246_000_000_000_000));
+const STATIC_CONRTACT_SALT_LENGTH: u32 = 32;
 
 pub async fn setup_node_and_client() -> dhatu::types::MandalaClient {
     let client = MandalaClient::dev()
@@ -45,97 +47,99 @@ pub async fn setup_node_and_client() -> dhatu::types::MandalaClient {
     client
 }
 
+fn generate_salt() -> Vec<u8> {
+    let rng = thread_rng();
+
+    let random_string: String = rng
+        .sample_iter(&Alphanumeric)
+        .take(STATIC_CONRTACT_SALT_LENGTH as usize)
+        .map(char::from)
+        .collect();
+
+    let salt_string = hex::encode(random_string);
+    let salt = hex::decode(salt_string).expect("static values are valid");
+
+    salt
+}
+
 pub async fn setup_dummy_721_contract(client: &MandalaClient) -> subxt::utils::AccountId32 {
-    // let contract_code = std::fs::read("tests/common/erc721.wasm").expect("should read wasm file");
+    let contract_code = std::fs::read("tests/common/erc721.wasm").expect("should read wasm file");
 
-    // let tx_payload =
-    //     test_types::api::tx()
-    //         .contracts()
-    //         .upload_code(contract_code, None, Determinism::Enforced);
+    let tx_payload =
+        test_types::api::tx()
+            .contracts()
+            .upload_code(contract_code, None, Determinism::Enforced);
 
-    // let signer = sp_keyring::Sr25519Keyring::Alice.pair();
-    // let signer: PairSigner<PolkadotConfig, sp_core::sr25519::Pair> = PairSigner::new(signer);
+    let signer = sp_keyring::Sr25519Keyring::Alice.pair();
+    let signer: PairSigner<PolkadotConfig, sp_core::sr25519::Pair> = PairSigner::new(signer);
 
-    // let upload_code = client
-    //     .inner()
-    //     .tx()
-    //     .sign_and_submit_then_watch_default(&tx_payload, &signer)
-    //     .await
-    //     .expect("should deploy a new dummy contract transaction successfuly! ")
-    //     .wait_for_finalized_success()
-    //     .await
-    //     .expect("should upload contract successfully");
+    let upload_code = client
+        .inner()
+        .tx()
+        .sign_and_submit_then_watch_default(&tx_payload, &signer)
+        .await
+        .expect("should deploy a new dummy contract transaction successfuly! ")
+        .wait_for_finalized_success()
+        .await
+        .expect("should upload contract successfully");
 
-    // let static_code_hash =
-    //     hex::decode("7348c083c5fea839b2f9d1929cf0350d35840692f052ba58129890170a505588")
-    //         .expect("static values are valid");
+    let static_code_hash =
+        hex::decode("7348c083c5fea839b2f9d1929cf0350d35840692f052ba58129890170a505588")
+            .expect("static values are valid");
 
-    // println!("code hash size : {}", static_code_hash.len());
+    println!("code hash size : {}", static_code_hash.len());
 
-    // let static_code_hash = subxt::utils::H256::from_slice(static_code_hash.as_ref());
-    // let static_code_hash_event = CodeStored {
-    //     code_hash: static_code_hash,
-    // };
+    let static_code_hash = subxt::utils::H256::from_slice(static_code_hash.as_ref());
+    let static_code_hash_event = CodeStored {
+        code_hash: static_code_hash,
+    };
 
-    // let contract_code = upload_code
-    //     .find_first::<contracts::events::CodeStored>()
-    //     .unwrap()
-    //     .unwrap_or(static_code_hash_event);
-    // println!("contract code hash: {:?}", contract_code.code_hash);
+    let contract_code = upload_code
+        .find_first::<contracts::events::CodeStored>()
+        .unwrap()
+        .unwrap_or(static_code_hash_event);
+    println!("contract code hash: {:?}", contract_code.code_hash);
 
-    // let instantiate_payload = test_types::api::tx().contracts().instantiate(
-    //     0,
-    //     STATIC_GAS_LIMIT,
-    //     Some(Compact(9000_000_000_000000)),
-    //     contract_code.code_hash,
-    //     vec![],
-    //     vec![],
-    // );
+    const CONSTRUCTOR_SELECTOR: &str = "0x9bae9d5e";
+    let mut constructor_selector = Selector::from_raw(CONSTRUCTOR_SELECTOR).unwrap();
 
-    // let signer = sp_keyring::Sr25519Keyring::Bob.pair();
-    // let signer: PairSigner<PolkadotConfig, sp_core::sr25519::Pair> = PairSigner::new(signer);
+    let mut calldata = Vec::new();
 
-    // let instantiate = client
-    //     .inner()
-    //     .tx()
-    //     .sign_and_submit_then_watch_default(&instantiate_payload, &signer)
-    //     .await
-    //     .expect("should instantiate a new dummy contract transaction successfuly! ")
-    //     .wait_for_finalized_success()
-    //     .await;
+    calldata.append(&mut constructor_selector.encoded());
 
-    // let instantiate = match instantiate {
-    //     Ok(v) => v,
-    //     Err(e) => match e {
-    //         subxt::Error::Runtime(e) => match e {
-    //             subxt::error::DispatchError::Other => panic!(" other error "),
-    //             subxt::error::DispatchError::CannotLookup => panic!(" cannot lookup "),
-    //             subxt::error::DispatchError::BadOrigin => panic!(" bad origin "),
-    //             subxt::error::DispatchError::Module(e) => panic!("module error : {e}"),
-    //             subxt::error::DispatchError::ConsumerRemaining => panic!(" consumer remaining "),
-    //             subxt::error::DispatchError::NoProviders => panic!(" no providers "),
-    //             subxt::error::DispatchError::TooManyConsumers => panic!(" too many consumers "),
-    //             subxt::error::DispatchError::Token(_) => panic!(" token error "),
-    //             subxt::error::DispatchError::Arithmetic(_) => panic!(" arithmetic error "),
-    //             subxt::error::DispatchError::Transactional(_) => panic!(" transactional error "),
-    //             subxt::error::DispatchError::Exhausted => panic!(" exhausted "),
-    //             subxt::error::DispatchError::Corruption => panic!(" corruption "),
-    //             subxt::error::DispatchError::Unavailable => panic!(" unavailable "),
-    //             _ => todo!(),
-    //         },
-    //         _ => todo!(),
-    //     },
-    // };
+    let salt = generate_salt();
 
-    // let contract = instantiate
-    //     .find_first::<Instantiated>()
-    //     .expect("should emit instantiated event")
-    //     .expect("should find instantiated event");
+    let instantiate_payload = test_types::api::tx().contracts().instantiate(
+        0,
+        STATIC_GAS_LIMIT,
+        Some(Compact(9000_000_000_000000)),
+        contract_code.code_hash,
+        calldata,
+        salt,
+    );
 
-    // contract.contract
+    let signer = sp_keyring::Sr25519Keyring::Bob.pair();
+    let signer: PairSigner<PolkadotConfig, sp_core::sr25519::Pair> = PairSigner::new(signer);
 
-    AccountId32::from_str("5G6Y6DQHnaZD1c428uxYnkeUAND8Me59KEvB9gXzviA9p29g")
-        .expect("static value are valid")
+    let instantiate = client
+        .inner()
+        .tx()
+        .sign_and_submit_then_watch_default(&instantiate_payload, &signer)
+        .await
+        .expect("should instantiate a new dummy contract transaction successfuly! ")
+        .wait_for_finalized_success()
+        .await
+        .expect("should instantiate contract successfully");
+
+    let contract = instantiate
+        .find_first::<Instantiated>()
+        .expect("should emit instantiated event")
+        .expect("should find instantiated event");
+
+    contract.contract
+
+    // AccountId32::from_str("5G6Y6DQHnaZD1c428uxYnkeUAND8Me59KEvB9gXzviA9p29g")
+    //     .expect("static value are valid")
 }
 
 impl WrappedExtrinsic<contracts::calls::types::Call>
