6929420317fdc865506e3ef6f426611bffdb3dc8
zian <mynameiszian@gmail.com>
2023-06-22 14:00:32 +0800

    done testing exstrinsic facade

--- dhatu/src/tx/extrinsics/extrinsics_tracker/tracker.rs
+++ dhatu/src/tx/extrinsics/extrinsics_tracker/tracker.rs
@@ -79,3 +79,66 @@ impl ExtrinsicWatcher {
         inner.insert(tx.id(), tx);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::registrar::key_manager::prelude::PublicAddress;
+    use crate::registrar::signer::TxBuilder;
+    use crate::tx::extrinsics::extrinsics_submitter::ExtrinsicSubmitter;
+    use crate::tx::extrinsics::manager::facade::ExtrinsicFacade;
+    use crate::types::MandalaConfig;
+    use crate::types::MandalaExtrinsics;
+    use std::str::FromStr;
+    use std::sync::mpsc;
+    pub(crate) use subxt::OnlineClient;
+
+    fn mock_pair() -> sp_core::sr25519::Pair {
+        sp_keyring::Sr25519Keyring::Alice.pair()
+    }
+
+    async fn mock_client() -> crate::types::NodeClient {
+        OnlineClient::<MandalaConfig>::new().await.unwrap()
+    }
+
+    async fn create_tx_progress() -> MandalaTransactionProgress {
+        let address = "5DJk1gegyQJk6BNs7LceZ1akt5e9fpm4gUYGzcfpKaLG9Mmb";
+        let new_address = PublicAddress::from_str(address).unwrap();
+        let pair = mock_pair();
+        let node_client = mock_client().await;
+
+        let value = 10000;
+        // Create the payload using the `construct` function from `BalanceTransfer`
+        let payload = crate::tx::extrinsics::prelude::transfer_balance::constructor::BalanceTransfer::construct(new_address, value);
+        let extrinsic = TxBuilder::signed(&node_client.into(), pair, payload)
+            .await
+            .unwrap()
+            .0;
+
+        // Create a mock MandalaExtrinsics object
+        let tx = MandalaExtrinsics::new(extrinsic);
+        let tx_progress = ExtrinsicSubmitter::submit(tx).await.unwrap();
+
+        tx_progress
+    }
+
+    #[tokio::test]
+    async fn should_watch_unwatch_and_check_tx() {
+        let tx_progress = create_tx_progress().await;
+
+        let watcher = ExtrinsicWatcher::new();
+        let tx_id = watcher.watch(tx_progress, None, None).await;
+
+        let status = watcher.check(&tx_id).await.unwrap();
+
+        match status {
+            ExtrinsicStatus::Pending => {}
+            _ => panic!("should be pending"),
+        }
+
+        watcher.stop_watching(&tx_id).await;
+
+        let status = watcher.check(&tx_id).await;
+        assert!(status.is_none());
+    }
+}
